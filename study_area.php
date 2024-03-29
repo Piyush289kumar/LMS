b53392c1aa36c1e45330b5a2bf877341e12996e7
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-29 21:32:08 +0530

    Study Area Building

--- study_area.php
+++ study_area.php
@@ -42,14 +42,12 @@
   </script>";
   }
   $course_id = $_GET['course_id'];
-
   if (!isset($_GET['course_id'])) {
     $chapter_id = 'blank';
   } else {
     $chapter_id = $_GET['chapter_id'];
   }
   ?>
-
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center" style="background: #0A2640;">
     <div class="d-flex align-items-center justify-content-between">
@@ -78,7 +76,6 @@
           <span>Study Area</span>
         </a>
       </li>
-
       <!-- Fetch Chapter Data -->
       <!-- Fetch All Chapter From Database -->
       <?php
@@ -105,37 +102,34 @@
             </ul>
           </li>
           <!-- Chapter Nav -->
-
-
       <?php
           $idx++;
         }
       } ?>
       <!-- Fetch Chapter Data -->
-
     </ul>
   </aside><!-- End Sidebar-->
-
   <!-- Fetch Chapter Details as chooses -->
   <?php
-                  $sql_fetch_chapter = "SELECT * FROM chapter
+  $sql_fetch_chapter = "SELECT * FROM chapter
                   INNER JOIN course ON chapter.course_id = course.cid
                   WHERE chapter.active_record = 'Yes'
                   AND course.active_record = 'Yes'
+                  AND chapter.chapter_id = '{$chapter_id}'
                   AND chapter.course_id = '{$course_id}'
                   ORDER BY chapter.chapter_index";
-
   echo $result_sql_fetch_chapter = mysqli_query($conn, $sql_fetch_chapter) or die("Query Failed!!");
   if (mysqli_num_rows($result_sql_fetch_chapter) > 0) {
     while ($row_sql_fetch_chapter = mysqli_fetch_assoc($result_sql_fetch_chapter)) {
   ?>
       <main id="main" class="main">
         <div class="pagetitle">
-          <h1>Dashboard</h1>
+          <h1>Study Area</h1>
           <nav>
             <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
+              <li class="breadcrumb-item"><a>Course: <?php echo $row_sql_fetch_chapter['title'] ?></a></li>
+              <li class="breadcrumb-item"><a>Chapter: <?php echo $row_sql_fetch_chapter['chapter_index'] ?></a></li>
+              <li class="breadcrumb-item active"><?php echo $row_sql_fetch_chapter['chapter_title'] ?></li>
             </ol>
           </nav>
         </div><!-- End Page Title -->
@@ -148,13 +142,13 @@
                 <div class="col-md-12">
                   <div class="card info-card sales-card">
                     <div class="card-body">
-                      <h5 class="card-title">Sales <span>| Today</span></h5>
+                      <h5 class="card-title"><?php echo $row_sql_fetch_chapter['title'] ?> <span>| Chapter: <?php echo $row_sql_fetch_chapter['chapter_index'] ?></span></h5>
                       <div class="d-flex align-items-center">
                         <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                           <i class="bi bi-bookmarks"></i>
                         </div>
                         <div class="ps-3">
-                          <h6>Chapter: <?php echo $row_sql_fetch_chapter['chapter_title'] ?></h6>
+                          <h6><?php echo $row_sql_fetch_chapter['chapter_title'] ?></h6>
                         </div>
                       </div>
                     </div>
@@ -165,44 +159,9 @@
                   <div class="card info-card sales-card">
                     <div class="card-body">
                       <div class="card-text px-3 py-4">
-                        Top 10 Excel Pivot Table Keyboard Shortcuts
-                        Using programs like Microsoft Excel, pivot tables are handy for highlighting key data. They let you
-                        “pivot,” changing data direction for fresh insights. By grouping similar data and applying
-                        operations
-                        like total or average, pivot tables summarize large datasets efficiently. However, dealing with
-                        massive data can be tedious. Knowing keyboard shortcuts becomes valuable, speeding up common tasks
-                        and
-                        saving time. These shortcuts enhance work speed and reduce the likelihood of mistakes, common in
-                        manual processes. Tools also minimize human errors like choosing the wrong item or pushing the wrong
-                        button, improving overall efficiency and accuracy in data handling.
-                        Excel Pivot Table Shortcuts
-                        Shortcuts
-                        Action
-                        Alt + N + V + T + Enter
-                        Create a PivotTable from selected data
-                        Alt + Shift + F10
-                        Opens the context menu for the selected cell in the PivotTable
-                        Up/Down/Left/Right arrows
-                        Navigate within the context menu
-                        Enter
-                        Selects the highlighted option in the context menu
-                        Tab
-                        Switch between the PivotTable fields list and the data area
-                        Alt + Down Arrow
-                        Opens the dropdown menu for a cell with options to filter data
-                        Up/Down arrows
-                        Select options within the dropdown menu
-                        Enter
-                        Apply the selected filter
-                        Esc
-                        Close the dropdown menu without applying any filter
-                        Ctrl + Tab
-                        Switch between worksheets
-                        Ctrl + Alt + F5
-                        Refresh all PivotTables in the workbook Refresh all PivotTables in the workbook
-                        Shortcut to Create a Pivot Table From Data
-                        You can quickly make a Pivot Table in Excel with the data you choose by pressing this keyboard
-                        button.
+                        <!-- CKEditor -->
+                        <textarea id="editor" class="form-control"><?php echo $row_sql_fetch_chapter['chapter_description'] ?></textarea>
+                        <!-- CKEditor -->
                       </div>
                     </div>
                   </div>
@@ -212,17 +171,14 @@
           </div>
         </section>
       </main><!-- End #main -->
-
     <?php }
   } else {
     ?>
-
     <div class="row" style='height: 80vh;'>
       <div class="offset-lg-5 d-flex justify-content-center align-items-center col-lg-4">
         <a class="btn btn-danger w-75">No Data Found.</a>
       </div>
     </div>
-
   <?php
   } ?>
   <!-- ======= Footer ======= --><!-- ======= Footer ======= -->
@@ -234,12 +190,8 @@
     <div class="credits">
       Designed & Developed by <a href="https://github.com/Piyush289kumar/">Piyush Kumar Raikwar</a>
     </div>
-
   </footer><!-- End Footer -->
-
   <a class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-
   <!-- Vendor JS Files -->
   <script src="admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -249,11 +201,22 @@
   <script src="admin/assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="admin/assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="admin/assets/vendor/php-email-form/validate.js"></script>
-
   <!-- Template Main JS File -->
   <script src="admin/assets/js/main.js"></script>
-
-
+  <!-- ckeditor -->
+  <script src="https://cdn.ckeditor.com/ckeditor5/41.0.0/classic/ckeditor.js"></script>
+  <script>
+    ClassicEditor
+      .create(document.querySelector('#editor'), {
+        toolbar: ['']
+      })
+      .then(editor => {
+        editor.enableReadOnlyMode('#editor');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  </script>
 </body>
 
 </html>
