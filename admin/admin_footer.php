3d36166149281be76ff9c2340705d0e93a8d54a5
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-29 20:36:09 +0530

    Study Area Building

--- admin/admin_footer.php
+++ admin/admin_footer.php
@@ -1,14 +1,14 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
-    <!-- <div class="copyright">
+  <!-- <div class="copyright">
         &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
     </div> -->
-      <!-- Import Copyright File -->
+  <!-- Import Copyright File -->
   <?php include('copyright/copyright.php') ?>
 
 </footer><!-- End Footer -->
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<a class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 
 <?php include('admin_footer_vendor_links.php') ?>
--- study_area.php
+++ study_area.php
@@ -41,7 +41,15 @@
     window.location.href = '$hostname/courses.php';
   </script>";
   }
-  $course_id = $_GET['course_id']; ?>
+  $course_id = $_GET['course_id'];
+
+  if (!isset($_GET['course_id'])) {
+    $chapter_id = 'blank';
+  } else {
+    $chapter_id = $_GET['chapter_id'];
+  }
+  ?>
+
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center" style="background: #0A2640;">
     <div class="d-flex align-items-center justify-content-between">
@@ -85,12 +93,12 @@
       ?>
           <!-- Chapter -->
           <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#chapter_<?php echo $idx ?>" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#chapter_<?php echo $idx ?>" data-bs-toggle="collapse" style='cursor: pointer;'>
               <i class="bi bi-menu-button-wide"></i><span>Chapter: <?php echo $idx ?></span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
             <ul id="chapter_<?php echo $idx ?>" class="nav-content collapse" data-bs-parent="#sidebar-nav">
               <li>
-                <a href="components-alerts.html">
+                <a href="study_area.php?course_id=<?php echo $course_id ?>&chapter_id=<?php echo $row_sql_fetch_all_chapter['chapter_id'] ?>">
                   <i class="bi bi-circle"></i><span><?php echo $row_sql_fetch_all_chapter['chapter_title'] ?></span>
                 </a>
               </li>
@@ -107,104 +115,131 @@
 
     </ul>
   </aside><!-- End Sidebar-->
-  <main id="main" class="main">
-    <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
-    <section class="section dashboard">
-      <div class="row">
-        <!-- Left side columns -->
-        <div class="col-md-12">
-          <div class="row">
-            <!-- Header Card -->
-            <div class="col-md-12">
-              <div class="card info-card sales-card">
-                <div class="card-body">
-                  <h5 class="card-title">Sales <span>| Today</span></h5>
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-bookmarks"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>React Context API</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div><!-- End Header Card -->
-            <!-- Contend Card -->
-            <div class="col-md-12">
-              <div class="card info-card sales-card">
-                <div class="card-body">
-                  <div class="card-text px-3 py-4">
-                    Top 10 Excel Pivot Table Keyboard Shortcuts
-                    Using programs like Microsoft Excel, pivot tables are handy for highlighting key data. They let you
-                    “pivot,” changing data direction for fresh insights. By grouping similar data and applying
-                    operations
-                    like total or average, pivot tables summarize large datasets efficiently. However, dealing with
-                    massive data can be tedious. Knowing keyboard shortcuts becomes valuable, speeding up common tasks
-                    and
-                    saving time. These shortcuts enhance work speed and reduce the likelihood of mistakes, common in
-                    manual processes. Tools also minimize human errors like choosing the wrong item or pushing the wrong
-                    button, improving overall efficiency and accuracy in data handling.
-                    Excel Pivot Table Shortcuts
-                    Shortcuts
-                    Action
-                    Alt + N + V + T + Enter
-                    Create a PivotTable from selected data
-                    Alt + Shift + F10
-                    Opens the context menu for the selected cell in the PivotTable
-                    Up/Down/Left/Right arrows
-                    Navigate within the context menu
-                    Enter
-                    Selects the highlighted option in the context menu
-                    Tab
-                    Switch between the PivotTable fields list and the data area
-                    Alt + Down Arrow
-                    Opens the dropdown menu for a cell with options to filter data
-                    Up/Down arrows
-                    Select options within the dropdown menu
-                    Enter
-                    Apply the selected filter
-                    Esc
-                    Close the dropdown menu without applying any filter
-                    Ctrl + Tab
-                    Switch between worksheets
-                    Ctrl + Alt + F5
-                    Refresh all PivotTables in the workbook Refresh all PivotTables in the workbook
-                    Shortcut to Create a Pivot Table From Data
-                    You can quickly make a Pivot Table in Excel with the data you choose by pressing this keyboard
-                    button.
-                  </div>
-                </div>
-              </div>
-            </div><!-- End Contend Card -->
-          </div>
-        </div><!-- End Left side columns -->
-      </div>
-    </section>
-  </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
+  <!-- Fetch Chapter Details as chooses -->
+  <?php
+                  $sql_fetch_chapter = "SELECT * FROM chapter
+                  INNER JOIN course ON chapter.course_id = course.cid
+                  WHERE chapter.active_record = 'Yes'
+                  AND course.active_record = 'Yes'
+                  AND chapter.course_id = '{$course_id}'
+                  ORDER BY chapter.chapter_index";
+
+  echo $result_sql_fetch_chapter = mysqli_query($conn, $sql_fetch_chapter) or die("Query Failed!!");
+  if (mysqli_num_rows($result_sql_fetch_chapter) > 0) {
+    while ($row_sql_fetch_chapter = mysqli_fetch_assoc($result_sql_fetch_chapter)) {
+  ?>
+      <main id="main" class="main">
+        <div class="pagetitle">
+          <h1>Dashboard</h1>
+          <nav>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </nav>
+        </div><!-- End Page Title -->
+        <section class="section dashboard">
+          <div class="row">
+            <!-- Left side columns -->
+            <div class="col-md-12">
+              <div class="row">
+                <!-- Header Card -->
+                <div class="col-md-12">
+                  <div class="card info-card sales-card">
+                    <div class="card-body">
+                      <h5 class="card-title">Sales <span>| Today</span></h5>
+                      <div class="d-flex align-items-center">
+                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                          <i class="bi bi-bookmarks"></i>
+                        </div>
+                        <div class="ps-3">
+                          <h6>Chapter: <?php echo $row_sql_fetch_chapter['chapter_title'] ?></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div><!-- End Header Card -->
+                <!-- Contend Card -->
+                <div class="col-md-12">
+                  <div class="card info-card sales-card">
+                    <div class="card-body">
+                      <div class="card-text px-3 py-4">
+                        Top 10 Excel Pivot Table Keyboard Shortcuts
+                        Using programs like Microsoft Excel, pivot tables are handy for highlighting key data. They let you
+                        “pivot,” changing data direction for fresh insights. By grouping similar data and applying
+                        operations
+                        like total or average, pivot tables summarize large datasets efficiently. However, dealing with
+                        massive data can be tedious. Knowing keyboard shortcuts becomes valuable, speeding up common tasks
+                        and
+                        saving time. These shortcuts enhance work speed and reduce the likelihood of mistakes, common in
+                        manual processes. Tools also minimize human errors like choosing the wrong item or pushing the wrong
+                        button, improving overall efficiency and accuracy in data handling.
+                        Excel Pivot Table Shortcuts
+                        Shortcuts
+                        Action
+                        Alt + N + V + T + Enter
+                        Create a PivotTable from selected data
+                        Alt + Shift + F10
+                        Opens the context menu for the selected cell in the PivotTable
+                        Up/Down/Left/Right arrows
+                        Navigate within the context menu
+                        Enter
+                        Selects the highlighted option in the context menu
+                        Tab
+                        Switch between the PivotTable fields list and the data area
+                        Alt + Down Arrow
+                        Opens the dropdown menu for a cell with options to filter data
+                        Up/Down arrows
+                        Select options within the dropdown menu
+                        Enter
+                        Apply the selected filter
+                        Esc
+                        Close the dropdown menu without applying any filter
+                        Ctrl + Tab
+                        Switch between worksheets
+                        Ctrl + Alt + F5
+                        Refresh all PivotTables in the workbook Refresh all PivotTables in the workbook
+                        Shortcut to Create a Pivot Table From Data
+                        You can quickly make a Pivot Table in Excel with the data you choose by pressing this keyboard
+                        button.
+                      </div>
+                    </div>
+                  </div>
+                </div><!-- End Contend Card -->
+              </div>
+            </div><!-- End Left side columns -->
+          </div>
+        </section>
+      </main><!-- End #main -->
+
+    <?php }
+  } else {
+    ?>
+
+    <div class="row" style='height: 80vh;'>
+      <div class="offset-lg-5 d-flex justify-content-center align-items-center col-lg-4">
+        <a class="btn btn-danger w-75">No Data Found.</a>
+      </div>
+    </div>
+
+  <?php
+  } ?>
+  <!-- ======= Footer ======= --><!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
-    <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-    </div>
+    <!-- <div class="copyright">
+        &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+    </div> -->
+    <!-- Import Copyright File -->
     <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      Designed & Developed by <a href="https://github.com/Piyush289kumar/">Piyush Kumar Raikwar</a>
     </div>
+
   </footer><!-- End Footer -->
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <a class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
   <!-- Vendor JS Files -->
   <script src="admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -214,8 +249,11 @@
   <script src="admin/assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="admin/assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="admin/assets/vendor/php-email-form/validate.js"></script>
+
   <!-- Template Main JS File -->
   <script src="admin/assets/js/main.js"></script>
+
+
 </body>
 
 </html>
