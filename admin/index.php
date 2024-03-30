3b6d79099048ecbd1f2e42ba07f1f5aa14363838
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-30 17:40:20 +0530

    Working on admin index.php page

--- admin/index.php
+++ admin/index.php
@@ -165,12 +165,13 @@ include('private_files/system_configure_setting.php') ?>
                 </div>
               </div>
             </div><!-- End Reports -->
-            <!-- Recent Sales -->
+
+            <!-- Start Recent Courses -->
             <div class="col-12">
               <div class="card recent-sales overflow-auto">
                 <div class="card-body">
                   <h5 class="card-title">Recent Courses</h5>
-                  <table class="table table-borderless datatable">
+                  <table class="table table-borderless datatable table-striped">
                     <thead>
                       <tr>
                         <th class="border border-primary text-primary">Title</th>
@@ -201,134 +202,103 @@ include('private_files/system_configure_setting.php') ?>
                   </table>
                 </div>
               </div>
-            </div><!-- End Recent Sales -->
-            <!-- Top Selling -->
+            </div><!-- End Recent Courses -->
+
+
+            <!-- Start Recent Users -->
             <div class="col-12">
-              <div class="card top-selling overflow-auto">
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-                <div class="card-body pb-0">
-                  <h5 class="card-title">Top Selling <span>| Today</span></h5>
-                  <table class="table table-borderless">
+              <div class="card recent-sales overflow-auto">
+                <div class="card-body">
+                  <h5 class="card-title">Recent Users</h5>
+                  <table class="table table-borderless datatable table-striped">
                     <thead>
                       <tr>
-                        <th scope="col">Preview</th>
-                        <th scope="col">Product</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Sold</th>
-                        <th scope="col">Revenue</th>
+                        <th class="border border-primary text-primary">User Name</th>
+                        <th class="border border-primary text-primary">Full Name</th>
+                        <th class="border border-primary text-primary">Designation</th>
+                        <th class="border border-primary text-primary">Role</th>
+                        <th class="border border-primary text-primary">Phone</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-1.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Ut inventore ipsa voluptas nulla</a></td>
-                        <td>$64</td>
-                        <td class="fw-bold">124</td>
-                        <td>$5,828</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-2.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Exercitationem similique doloremque</a></td>
-                        <td>$46</td>
-                        <td class="fw-bold">98</td>
-                        <td>$4,508</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-3.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Doloribus nisi exercitationem</a></td>
-                        <td>$59</td>
-                        <td class="fw-bold">74</td>
-                        <td>$4,366</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-4.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Officiis quaerat sint rerum error</a></td>
-                        <td>$32</td>
-                        <td class="fw-bold">63</td>
-                        <td>$2,016</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><a href="#"><img src="assets/img/product-5.jpg" alt=""></a></th>
-                        <td><a href="#" class="text-primary fw-bold">Sit unde debitis delectus repellendus</a></td>
-                        <td>$79</td>
-                        <td class="fw-bold">41</td>
-                        <td>$3,239</td>
-                      </tr>
+                      <!-- Fetch Users Data -->
+                      <?php
+                      $sql_fetch_all_user = "SELECT * FROM user_data WHERE active_record = 'Yes' ORDER BY user_id DESC";
+                      $result_sql_fetch_all_user = mysqli_query($conn, $sql_fetch_all_user) or die("Query Failed!!");
+                      if (mysqli_num_rows($result_sql_fetch_all_user) > 0) {
+                        while ($row_sql_fetch_all_user = mysqli_fetch_assoc($result_sql_fetch_all_user)) {
+                      ?>
+                          <tr>
+                            <td><?php echo $row_sql_fetch_all_user['username'] ?></td>
+                            <td><?php echo $row_sql_fetch_all_user['full_name'] ?></td>
+                            <td><?php echo $row_sql_fetch_all_user['designation'] ?></td>
+                            <!-- Role Permission Conditional Rending -->
+                            <?php
+                            $user_role_permission = '';
+                            if ($row_sql_fetch_all_user['role'] == '0') {
+                              $user_role_permission = 'Administration Level';
+                            } elseif ($row_sql_fetch_all_user['role'] == '1') {
+                              $user_role_permission = 'Middle Management Level';
+                            } else {
+                              $user_role_permission = 'End User';
+                            }
+                            ?>
+                            <td><?php echo $user_role_permission ?></td>
+                            <td><?php echo $row_sql_fetch_all_user['phone'] ?></td>
+                          </tr>
+                      <?php }
+                      } ?>
                     </tbody>
                   </table>
                 </div>
               </div>
-            </div><!-- End Top Selling -->
+            </div><!-- End Recent Users -->
+
           </div>
         </div><!-- End Left side columns -->
         <!-- Right side columns -->
         <div class="col-lg-4">
           <!-- Recent Activity -->
           <div class="card">
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div>
+
             <div class="card-body">
               <h5 class="card-title">Recent Activity <span>| Today</span></h5>
               <div class="activity">
+
                 <div class="activity-item d-flex">
                   <div class="activite-label">32 min</div>
                   <i class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
                   <div class="activity-content">
-                    Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae
+                    Create A New <a class="fw-bold text-dark"> Course</a>
                   </div>
                 </div><!-- End activity item-->
+
                 <div class="activity-item d-flex">
                   <div class="activite-label">56 min</div>
                   <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
                   <div class="activity-content">
-                    Voluptatem blanditiis blanditiis eveniet
+                    Delete A End User
                   </div>
                 </div><!-- End activity item-->
                 <div class="activity-item d-flex">
                   <div class="activite-label">2 hrs</div>
                   <i class='bi bi-circle-fill activity-badge text-primary align-self-start'></i>
                   <div class="activity-content">
-                    Voluptates corrupti molestias voluptatem
+                    Alter User Permission
                   </div>
                 </div><!-- End activity item-->
                 <div class="activity-item d-flex">
                   <div class="activite-label">1 day</div>
                   <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
                   <div class="activity-content">
-                    Tempore autem saepe <a href="#" class="fw-bold text-dark">occaecati voluptatem</a> tempore
+                    Modify <a href="#" class="fw-bold text-dark">Category</a> Data
                   </div>
                 </div><!-- End activity item-->
                 <div class="activity-item d-flex">
-                  <div class="activite-label">2 days</div>
-                  <i class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
+                  <div class="activite-label">2 Day</div>
+                  <i class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
                   <div class="activity-content">
-                    Est sit eum reiciendis exercitationem
-                  </div>
-                </div><!-- End activity item-->
-                <div class="activity-item d-flex">
-                  <div class="activite-label">4 weeks</div>
-                  <i class='bi bi-circle-fill activity-badge text-muted align-self-start'></i>
-                  <div class="activity-content">
-                    Dicta dolorem harum nulla eius. Ut quidem quidem sit quas
+                    Create A New <a class="fw-bold text-dark"> Course</a>
                   </div>
                 </div><!-- End activity item-->
               </div>
@@ -404,19 +374,8 @@ include('private_files/system_configure_setting.php') ?>
           </div><!-- End Budget Report -->
           <!-- Website Traffic -->
           <div class="card">
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div>
             <div class="card-body pb-0">
-              <h5 class="card-title">Website Traffic <span>| Today</span></h5>
+              <h5 class="card-title">Application Traffic <span>| This Month</span></h5>
               <div id="trafficChart" style="min-height: 400px;" class="echart"></div>
               <script>
                 document.addEventListener("DOMContentLoaded", () => {
@@ -474,50 +433,7 @@ include('private_files/system_configure_setting.php') ?>
               </script>
             </div>
           </div><!-- End Website Traffic -->
-          <!-- News & Updates Traffic -->
-          <div class="card">
-            <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div>
-            <div class="card-body pb-0">
-              <h5 class="card-title">News &amp; Updates <span>| Today</span></h5>
-              <div class="news">
-                <div class="post-item clearfix">
-                  <img src="assets/img/news-1.jpg" alt="">
-                  <h4><a href="#">Nihil blanditiis at in nihil autem</a></h4>
-                  <p>Sit recusandae non aspernatur laboriosam. Quia enim eligendi sed ut harum...</p>
-                </div>
-                <div class="post-item clearfix">
-                  <img src="assets/img/news-2.jpg" alt="">
-                  <h4><a href="#">Quidem autem et impedit</a></h4>
-                  <p>Illo nemo neque maiores vitae officiis cum eum turos elan dries werona nande...</p>
-                </div>
-                <div class="post-item clearfix">
-                  <img src="assets/img/news-3.jpg" alt="">
-                  <h4><a href="#">Id quia et et ut maxime similique occaecati ut</a></h4>
-                  <p>Fugiat voluptas vero eaque accusantium eos. Consequuntur sed ipsam et totam...</p>
-                </div>
-                <div class="post-item clearfix">
-                  <img src="assets/img/news-4.jpg" alt="">
-                  <h4><a href="#">Laborum corporis quo dara net para</a></h4>
-                  <p>Qui enim quia optio. Eligendi aut asperiores enim repellendusvel rerum cuder...</p>
-                </div>
-                <div class="post-item clearfix">
-                  <img src="assets/img/news-5.jpg" alt="">
-                  <h4><a href="#">Et dolores corrupti quae illo quod dolor</a></h4>
-                  <p>Odit ut eveniet modi reiciendis. Atque cupiditate libero beatae dignissimos eius...</p>
-                </div>
-              </div><!-- End sidebar recent posts-->
-            </div>
-          </div><!-- End News & Updates -->
+
         </div><!-- End Right side columns -->
       </div>
     </section>
