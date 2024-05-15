5710bebfa8020d9a3f2c016b8258f42989c272d1
Piyush Kumar Raikwar <94155141+Piyush289kumar@users.noreply.github.com>
2024-05-15 22:38:33 +0530

    Update copyright.php

--- copyright/copyright.php
+++ copyright/copyright.php
@@ -1,3 +1,3 @@
 <div class="credits">
-    Designed & Developed by <a href="https://github.com/Piyush289kumar/">Piyush Kumar Raikwar</a>
+    Designed & Developed by <a href="#">Aakansha Pandey</a>
 </div>
